7601d2f1feb7fa5bf6ce6bad8f3f9dcee1017f2b
rxctp <9989573@qq.com>
2017-06-01 20:36:58 +0800

    post

--- config/function.php
+++ config/function.php
@@ -85,6 +85,14 @@ function shadowsocks_create($pid,$passwd,$traffic){
       );
     }
   }
+ $data = [
+      'action' => 'create',
+      'pid'=>$pid,
+      'password'=>$passwd
+  ];
+  $uri = 'http://104.197.233.113/shadowsocksapi.php';
+  curlPost($uri,$data);
+   
   return $result;
 }
 
@@ -378,5 +386,19 @@ function shadowsocks_query($pid){
     }
   }
   return $result;
+}
+function curlPost($uri, $data)
+{
+    $ch     = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $uri);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    $return = curl_exec($ch);
+
+    curl_close($ch);
+    return $return;
 }
 
